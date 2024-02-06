8b60da9462f318b726d95f351d1ea780c73f786c
sojibdasjr <contact.sojibdas@gmail.com>
2024-02-06 11:14:20 +0600

    connect to DB

--- api/server.js
+++ api/server.js
@@ -1,8 +1,16 @@
 import { log } from "console";
 import express from "express";
+import mongoose from "mongoose";
+
+
+mongoose.connect('mongodb+srv://mern-auth:mern-auth@mern.4xp8tv7.mongodb.net/mern-auth?retryWrites=true&w=majority').then(()=>{
+    log("Connected to MongoDB");
+}).catch((err)=>{
+    log(err);
+})
 
 const app = express ();
 
 app.listen(3000, ()=>{
-    log('Server listening to on port 3000 !');
+    log('Server listening to on port 3000');
 });
